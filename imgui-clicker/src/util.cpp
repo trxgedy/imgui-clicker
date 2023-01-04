f54fd9dd2f7b77ea9fd7697579962cbe23188787
tragedy <79763373+trxgedy@users.noreply.github.com>
2023-01-04 12:05:32 -0300

    Update util.cpp

--- imgui-clicker/src/util.cpp
+++ imgui-clicker/src/util.cpp
@@ -1,29 +1,6 @@
 #include "stdafx.hpp"
 #include "util.hpp"
 
-auto c_util::pid( std::string_view p_name ) -> DWORD
-{
-	const auto handle { CreateToolhelp32Snapshot( TH32CS_SNAPPROCESS, NULL ) };
-	PROCESSENTRY32 entry; entry.dwSize = sizeof( entry );
-
-	if ( !Process32First( handle, &entry ) )
-		return 0;
-
-	do
-	{
-		if ( p_name == entry.szExeFile )
-		{
-			CloseHandle( handle );
-			return entry.th32ProcessID;
-		}
-
-	} while ( Process32Next( handle, &entry ) );
-
-	CloseHandle( handle );
-	return 0;
-}
-
-
 auto c_util::is_javaw_foreground() -> bool
 {
 	HWND hwnd { FindWindowA( "LWJGL", nullptr ) };
